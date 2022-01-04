b5cc6a8fbe54b1482be6f5c7f44908b103d8eb98
Daniel Staps <daniel.staps97@gmail.com>
2022-01-04 15:45:11 +0100

    adding test to shapes dataset and all garbage classes = num classes

--- prototorch_oneclass/datasets/shapes.py
+++ prototorch_oneclass/datasets/shapes.py
@@ -51,11 +51,11 @@ def create_shapes(num_samples=300,
             dataset.append(types[kwargs['kind']])
 
     datasets, targets = zip(*dataset)
+    targets = np.where(targets[0] <= num_classes, targets[0], num_classes)
     return datasets, targets
 
 
 class Shapes(torch.utils.data.TensorDataset):
-
     def __init__(self,
                  num_samples: int = 300,
                  num_shapes: int = 1,
@@ -68,3 +68,38 @@ class Shapes(torch.utils.data.TensorDataset):
         print(self.data.shape)
         self.target = torch.squeeze(torch.LongTensor(y))
         super().__init__(self.data, self.target)
+
+
+if __name__ == '__main__':
+    # Configuration
+    kind = 'blobs'
+    num_samples = 1000
+    num_shapes = 1
+    num_classes = 1
+    outliers = 0.1
+    center = [[-0.1, 0.15], [0.1, 0.1], [-0.3, 0.1]]
+    cluster_std = [0.08, 0.05, 0.02]
+    random_state = 421
+    noise = 0.15
+    factor = 0.8
+    prototypes_per_class = 1
+
+    kwargs = {
+        'kind': kind,
+        'centers': center,
+        'cluster_std': cluster_std,
+        'random_state': random_state,
+        'noise': noise,
+        'factor': factor,
+    }
+
+    # Dataset
+    train_ds = Shapes(
+        num_samples=num_samples,
+        num_shapes=num_shapes,
+        num_classes=num_classes,
+        outliers=outliers,
+        **kwargs,
+    )
+
+    print(train_ds.data, train_ds.target)
