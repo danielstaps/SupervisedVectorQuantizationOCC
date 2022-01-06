882c02ccecd1c60a01407e1a20b6effe60d29634
Daniel Staps <daniel.staps97@gmail.com>
2022-01-06 12:36:34 +0100

    make callback global avaiable

--- prototorch_oneclass/functions/__init__.py
+++ prototorch_oneclass/functions/__init__.py
@@ -1,3 +1,4 @@
+from prototorch_oneclass.functions.callbacks import ThetaCallback
 from prototorch_oneclass.functions.losses import brier_score, csi_soft_loss
 
-__all__ = ["csi_soft_loss", "brier_score"]
+__all__ = ["csi_soft_loss", "brier_score", "ThetaCallback"]
